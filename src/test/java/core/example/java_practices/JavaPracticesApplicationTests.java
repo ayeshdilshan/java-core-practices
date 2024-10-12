f57f3232fbaaa76b5b7acc508238a09861318cfe
ayeshj <ayeshj@allianz.lk>
2024-10-12 23:02:57 +0530

    initial commit for Core Java practice

---
+++ src/test/java/core/example/java_practices/JavaPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package core.example.java_practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
